61ac9f04b1004028435d135edab707103b67a2af
kenedii <55466055+kenedii@users.noreply.github.com>
2025-05-01 00:44:38 -0400

    Update build script

--- src/v1/build.sh
+++ src/v1/build.sh
@@ -4,7 +4,9 @@ nvcc -std=c++14 \
      kmeans_host.cu kmeans_v1.cu \
      -o libkmeans.so \
      -lcudart \
-     && \
+     -gencode arch=compute_61,code=sm_61
+
+# Generate PTX for compute_61
 nvcc --ptx kmeans_v1.cu \
-     -gencode arch=compute_61,code=sm_61 \
+     -arch=compute_61 \
      -o kmeans_kernels.ptx
